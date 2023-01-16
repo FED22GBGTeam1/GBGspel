9476bd8e63a66d5522c223e3af40471c18006c38
frontendsara <sara.karlsson1991@hotmail.com>
2023-01-16 16:34:31 +0100

    started a structure for item.ts and powerup.ts

---
+++ src/item.ts
@@ -0,0 +1,17 @@
+class Item extends Gameobject {
+
+    constructor(position: p5.Vector, size: p5.Vector, imagePath: string, velocity: number) {
+        super (position, size, imagePath, velocity)
+    }
+
+    public itemCollected() {
+
+    }
+
+    public update() {
+
+    }
+
+}
+
+
