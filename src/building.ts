b5d21ceb1568b48593fc4bde1fb715bfe0843162
jesnagbg <j.m.pettersson@hotmail.se>
2023-01-16 16:17:55 +0100

    added functions

--- src/building.ts
+++ src/building.ts
@@ -1,3 +1,5 @@
+// import { Gameobject } from './gameobject.ts'
+
 class Building extends Gameobject {
     // Gör att man kan stänga av dödligheten vid krock (powerup)
     private deadlyCollision: boolean;
@@ -5,5 +7,13 @@ class Building extends Gameobject {
     constructor(position: p5.Vector, size: p5.Vector, imagePath: string, velocity: number, deadlyCollision: boolean) {
         super(position, size, imagePath, velocity);
         this.deadlyCollision = deadlyCollision;
+    }
+
+    public update() {
+        
+    }
+
+    public draw() {
+        
     }
 }
