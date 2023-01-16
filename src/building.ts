edd4963b8b35a76532b44e384e4e631841558dc1
jesnagbg <j.m.pettersson@hotmail.se>
2023-01-16 16:05:13 +0100

    basic structure

---
+++ src/building.ts
@@ -0,0 +1,9 @@
+class Building extends Gameobject {
+    // Gör att man kan stänga av dödligheten vid krock (powerup)
+    private deadlyCollision: boolean;
+
+    constructor(position: p5.Vector, size: p5.Vector, imagePath: string, velocity: number, deadlyCollision: boolean) {
+        super(position, size, imagePath, velocity);
+        this.deadlyCollision = deadlyCollision;
+    }
+}
