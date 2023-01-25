f2b7bdf72fb399df75c65055f1b89451f88efa6d
danielvanpalo <daniel@grevensgastro.se>
2023-01-25 14:24:17 +0100

    up

--- src/sketch.ts
+++ src/sketch.ts
@@ -60,7 +60,7 @@ function preload() {
   images = {
     boat: loadImage('assets/boat.png'),
     buildings: loadImage('assets/building.png'),
-    controls: loadImage('assets/controls.png'),
+    controls: loadImage('assets/newcontrols.png'),
     fisk: loadImage('assets/fisk.jpg'),
     cloud1: loadImage('assets/cloud1.png'),
     cloud2: loadImage('assets/cloud2.png'),
